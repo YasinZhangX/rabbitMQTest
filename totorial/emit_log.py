d19c9fee7aae11a7aee9b0da79eb5ec07f9d7ef0
YasinZhang <271155397@qq.com>
2019-05-18 22:10:25 +0800

    finish tutorial 3 - Publish/Subscribe

---
+++ totorial/emit_log.py
@@ -0,0 +1,12 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.65.130'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+channel.basic_publish(exchange='logs', routing_key='', body=message)
+print(" [x] Sent %r" % message)
+connection.close()
